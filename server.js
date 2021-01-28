4250fbdb9248033d1e8bbb37005805b7ae1c7bec
ArtemRostomyan <71639600+ArtemRostomyan@users.noreply.github.com>
2021-01-28 20:59:13 +0300

    имитация сервера

---
+++ server.js
@@ -0,0 +1,182 @@
+[
+   {
+      "id": 'card-1',
+      "name": 'AMD Ryzen 5 2600',
+      "image": 'https://items.s1.citilink.ru/1068556_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 11390,
+      "type": 'processor',
+      "box": false,
+      "maker": 'AMD'
+   },
+   {
+      "id": 'card-2',
+      "name": 'AMD Ryzen 7 2700',
+      "image": 'https://items.s1.citilink.ru/1069277_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 12990,
+      "type": 'processor',
+      "box": false,
+      "maker": 'AMD'
+   },
+   {
+      "id": 'card-3',
+      "name": 'AMD Ryzen 5 3600',
+      "image": 'https://items.s1.citilink.ru/1151443_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 17890,
+      "type": 'processor',
+      "box": true,
+      "maker": 'AMD'
+   },
+   {
+      "id": 'card-4',
+      "name": 'AMD Ryzen 9 5950X',
+      "image": 'https://items.s1.citilink.ru/1431157_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 76990,
+      "type": 'processor',
+      "box": false,
+      "maker": 'AMD'
+   },
+   {
+      "id": 'card-5',
+      "name": 'AMD Ryzen Threadripper 3970X',
+      "image": 'https://items.s1.citilink.ru/1208856_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 187900,
+      "type": 'processor',
+      "box": true,
+      "maker": 'AMD'
+   },
+   {
+      "id": 'card-6',
+      "name": 'INTEL Core i3 9100F,',
+      "image": 'https://items.s1.citilink.ru/1163682_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 9390,
+      "type": 'processor',
+      "box": true,
+      "maker": 'Intel'
+   },
+   {
+      "id": 'card-7',
+      "name": 'INTEL Core i3 10100F',
+      "image": 'https://items.s1.citilink.ru/1426814_v02_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 7290,
+      "type": 'processor',
+      "box": false,
+      "maker": 'Intel'
+   },
+   {
+      "id": 'card-8',
+      "name": 'INTEL Core i9 10850K',
+      "image": 'https://items.s1.citilink.ru/1457522_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 39590,
+      "type": 'processor',
+      "box": false,
+      "maker": 'Intel'
+   },
+   {
+      "id": 'card-9',
+      "name": 'INTEL Core i9 10900',
+      "image": 'https://items.s1.citilink.ru/1369055_v01_b.jpg',
+      "description": 'Процессор для компьютера',
+      "price": 41990,
+      "type": 'processor',
+      "box": true,
+      "maker": 'Intel'
+   },
+   {
+      "id": 'card-10',
+      "name": 'GIGABYTE NV"ID"IA GeForce RTX 3070',
+      "image": 'https://items.s1.citilink.ru/1420798_v01_b.jpg',
+      "description": 'Видеок"арта "для компьютера',
+      "price": 89490,
+      "type": 'graphics-card',
+      "box": true,
+      "maker": 'GIGABYTE'
+   },
+   {
+      "id": 'card-11',
+      "name": 'PALIT NV"ID"IA GeForce RTX 3090',
+      "image": 'https://items.s1.citilink.ru/1432469_v01_b.jpg',
+      "description": 'Вид"еокар"та для компьютера',
+      "price": 216790,
+      "type": 'graphics-card',
+      "box": true,
+      "maker": 'PALIT'
+   },
+   {
+      "id": 'card-12',
+      "name": 'ASUS NV"ID"IA GeForce RTX 3090',
+      "image": 'https://items.s1.citilink.ru/1432012_v01_b.jpg',
+      "description": 'Ви"деока"рта для компьютера',
+      "price": 256490,
+      "type": 'graphics-card',
+      "box": true,
+      "maker": 'ASUS'
+   },
+   {
+      "id": 'card-13',
+      "name": 'ASUS nV"id"ia GeForce GT 710',
+      "image": 'https://items.s1.citilink.ru/1384278_v01_b.jpg',
+      "description": 'Ви"деока"рта для компьютера',
+      "price": 4030,
+      "type": 'graphics-card',
+      "box": true,
+      "maker": 'ASUS'
+   },
+   {
+      "id": 'card-14',
+      "name": 'MSI X470 GAMING PLUS MAX',
+      "image": 'https://items.s1.citilink.ru/1181657_v01_b.jpg',
+      "description": 'Материнская плата для компьютера',
+      "price": 8560,
+      "type": 'motherboard',
+      "box": true,
+      "maker": 'MSI'
+   },
+   {
+      "id": 'card-15',
+      "name": 'GIGABYTE Z490 UD',
+      "image": 'https://items.s1.citilink.ru/1372426_v02_b.jpg',
+      "description": 'Материнская плата для компьютера',
+      "price": 12300,
+      "type": 'motherboard',
+      "box": true,
+      "maker": 'GIGABYTE'
+   },
+   {
+      "id": 'card-16',
+      "name": 'ASROCK H310CM-DVS',
+      "image": 'https://items.s1.citilink.ru/1080835_v01_b.jpg',
+      "description": 'Материнская плата для компьютера',
+      "price": 4190,
+      "type": 'motherboard',
+      "box": true,
+      "maker": 'ASROCK'
+   },
+   {
+      "id": 'card-17',
+      "name": 'GIGABYTE GA-E6010N',
+      "image": 'https://items.s1.citilink.ru/1399200_v01_b.jpg',
+      "description": 'Материнская плата для компьютера',
+      "price": 3180,
+      "type": 'motherboard',
+      "box": true,
+      "maker": 'GIGABYTE'
+   },
+   {
+      "id": 'card-18',
+      "name": 'ASUS ROG MAXIMUS XII FORMULA',
+      "image": 'https://items.s1.citilink.ru/1375126_v08_b.jpg',
+      "description": 'Материнская плата для компьютера',
+      "price": 41840,
+      "type": 'motherboard',
+      "box": true,
+      "maker": 'ASUS'
+   }
+]
